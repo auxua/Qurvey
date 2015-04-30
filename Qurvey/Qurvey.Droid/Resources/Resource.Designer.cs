5c0ab9dd2741940998743b3bb5d31cb0e3e509ea
Tobias Räderscheidt <tobias@raederscheidt.de>
2015-04-30 17:39:17 +0200

    Initial commit Backend

--- Qurvey/Qurvey.Droid/Resources/Resource.Designer.cs
+++ Qurvey/Qurvey.Droid/Resources/Resource.Designer.cs
@@ -26,6 +26,8 @@ namespace Qurvey.Droid
 		
 		public static void UpdateIdValues()
 		{
+			global::Xamarin.Forms.Platform.Resource.String.ApplicationName = global::Qurvey.Droid.Resource.String.ApplicationName;
+			global::Xamarin.Forms.Platform.Resource.String.Hello = global::Qurvey.Droid.Resource.String.Hello;
 		}
 		
 		public partial class Attribute
@@ -62,6 +64,25 @@ namespace Qurvey.Droid
 			}
 			
 			private Drawable()
+			{
+			}
+		}
+		
+		public partial class String
+		{
+			
+			// aapt resource value: 0x7f030001
+			public const int ApplicationName = 2130903041;
+			
+			// aapt resource value: 0x7f030000
+			public const int Hello = 2130903040;
+			
+			static String()
+			{
+				global::Android.Runtime.ResourceIdManager.UpdateIdValues();
+			}
+			
+			private String()
 			{
 			}
 		}
