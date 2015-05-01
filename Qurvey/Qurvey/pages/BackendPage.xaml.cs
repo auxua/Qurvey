bfefca025bb2c6fff0ffc8679e3eaa327b3560e3
Tobias Räderscheidt <tobias@raederscheidt.de>
2015-05-01 16:13:53 +0200

    Calling a very basic web service

---
+++ Qurvey/Qurvey/pages/BackendPage.xaml.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using Qurvey.api;
+using Xamarin.Forms;
+using System.Threading.Tasks;
+
+namespace Qurvey.pages
+{
+	public partial class BackendPage : ContentPage
+	{
+		public BackendPage ()
+		{
+			InitializeComponent ();
+			getData ();
+		}
+
+		private async void getData() {
+			Task<string> data = RESTCalls.RestCallAsync<string>(null, "http://qurvey.raederscheidt.de/SurveyService.svc/Data/10", false);
+			dataLabel.Text = await data;	
+		}
+	}
+}
+
