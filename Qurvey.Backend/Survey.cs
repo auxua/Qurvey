92f8acf9a3e31b545bce48c3b27f5df183f9d5d5
Tobias Räderscheidt <tobias@raederscheidt.de>
2015-05-13 12:03:31 +0200

    Finished first Version of Survey, Answer, Vote data logic and wrote unit tests

--- Qurvey.Backend/Survey.cs
+++ Qurvey.Backend/Survey.cs
@@ -1,14 +1,13 @@
 ﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
-using System.Linq;
 using System.Runtime.Serialization;
-using System.Web;
 
 namespace Qurvey.Backend
 {
     [DataContract]
-    public class Survey
+    public class Survey : IEquatable<Survey>
     {
         [DataMember]
         [Key]
@@ -24,6 +23,51 @@ namespace Qurvey.Backend
         public DateTime Modified { get; set; }
 
         [DataMember]
-        public Answer[] Answers { get; set; } 
+        //public Answer[] Answers { get; set; }
+        public virtual ICollection<Answer> Answers { get; set; }
+
+        public bool Equals(Survey other)
+        {
+            return this.Id == other.Id;
+        }
+
+        protected Survey()
+        {
+          
+        }
+
+        public Survey(string question) : this()
+        {
+            Answers = new List<Answer>();
+            this.Created = this.Modified = DateTime.Now;
+            this.Question = question;
+        }
+
+        public void addAnswer(string answerText)
+        {
+            if (Answers == null)
+            {
+                Answers = new List<Answer>();
+            }
+            this.Answers.Add(new Answer(answerText));
+        }
+
+        public override bool Equals(object obj)
+        {
+            Survey other = obj as Survey;
+            if (other == null)
+            {
+                return false;
+            }
+            else
+            {
+                return this.Equals(other);
+            }
+        }
+
+        public override int GetHashCode()
+        {
+            return this.Id;
+        }
     }
 }
