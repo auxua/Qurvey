96ac7e40bea84766a7e6bb146146a5d5f36aa01f
Tobias Räderscheidt <tobias@raederscheidt.de>
2015-05-14 14:13:38 +0200

    Started to implement Backend interface in frontend

---
+++ Qurvey/Qurvey/api/Backend.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using Qurvey.Shared.Models;
+using System.Threading.Tasks;
+using Newtonsoft.Json;
+
+namespace Qurvey.api
+{
+	public class Backend
+	{
+		private async static Task<T1> CallBackendAsync<T1> (string endpoint, object input) {
+			string inp = JsonConvert.SerializeObject (input);
+
+			endpoint = "http://qurvey.raederscheidt.de/SurveyService.svc/" + endpoint;
+			Task<T1> res = RESTCalls.RestCallAsync<T1> (inp, endpoint, true); 
+			return await res;
+		}
+
+		public async static Task<string> SaveSurveyAsync(Survey survey) {
+			return await CallBackendAsync<string> ("savesurvey", survey);
+		}
+
+		public async static Task<string> DeleteSurveyAsync(Survey survey) {
+			return await CallBackendAsync<string> ("deletesurvey", survey);
+		}
+
+		public async static Task<Survey[]> GetSurveysAsync(string course) {
+			return await CallBackendAsync<Survey[]> ("getsurveys", course);
+		}
+	}
+}
