d4799e1fbff2d37b2badc12d3bfe3beb7cd54a86
Arno Schmetz <arno.schmetz@rwth-aachen.de>
2015-05-15 11:50:16 +0200

    adding Linking Navigation

---
+++ Qurvey/Qurvey/pages/CourseRoomPage.xaml.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+
+namespace Qurvey.pages
+{
+	public partial class CourseRoomPage : NavigationPage
+	{
+		public CourseRoomPage ()
+		{
+			InitializeComponent ();
+
+
+		}
+	}
+}
