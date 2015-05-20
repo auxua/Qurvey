9214ff894ff07d4da9eb3043b27747bb87a5365a
Tobias Räderscheidt <tobias@raederscheidt.de>
2015-05-20 17:25:27 +0200

    See last commit

---
+++ Qurvey.Backend.Test/TestSurveyContext.cs
@@ -0,0 +1,19 @@
+﻿using Qurvey.Shared.Models;
+using System.Data.Entity;
+
+namespace Qurvey.Backend.Test
+{
+    public class TestSurveyContext : SurveyContext
+    {
+        public new DbSet<Survey> Surveys
+        {
+            get { return base.Surveys; }
+            set { base.Surveys = value; }
+        }
+
+        public new DbSet<Vote> Votes {
+            get { return base.Votes; }
+            set { base.Votes = value; }
+        }
+    }
+}
