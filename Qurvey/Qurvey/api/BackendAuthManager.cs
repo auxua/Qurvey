571be380a69712ff96e728dafaa5c7c127dcd07b
Tobias Räderscheidt <tobias@raederscheidt.de>
2015-06-04 11:50:00 +0200

    Authentification with Backend on Login

---
+++ Qurvey/Qurvey/api/BackendAuthManager.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using Qurvey.Shared.Models;
+using Xamarin.Forms;
+
+namespace Qurvey.api
+{
+	public class BackendAuthManager
+	{
+		protected static BackendAuthManager instance;
+
+		public static BackendAuthManager Instance {
+			get {
+				if (instance == null) {
+					instance = new BackendAuthManager ();
+				}
+				return instance;
+			}
+		}
+
+		protected BackendAuthManager() {
+			User user = (User) Application.Current.Properties["User"];
+			if (user == null) {
+				IsAuthenticated = false;
+			} else {
+				IsAuthenticated = true;
+				User = user;
+			}
+		}
+
+		public bool IsAuthenticated { get; protected set; }
+
+		public User User { get; protected set; }
+
+		public void AuthenticateWithBackend() {
+			if (IsAuthenticated) {
+				throw new Exception ("User is already authenticated with backend");
+			}
+
+			User user = Backend.CreateNewUserAsync().Result;
+			Application.Current.Properties ["User"] = user;
+			Application.Current.SavePropertiesAsync();
+			User = user;
+			IsAuthenticated = true;
+		}
+	}
+}
+
